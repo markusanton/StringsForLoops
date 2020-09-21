3669cfa5e7e9fbae6a7e4556d2af89ca889e51e7
markusanton <markusa070@gmail.com>
2020-09-21 14:41:20 +0300

    task 2

---
+++ NameLengthComparison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NameLengthComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter name 1:");
+            string userNameOne = Console.ReadLine();
+
+            Console.WriteLine("Enter name 2:");
+            string userNameTwo = Console.ReadLine();
+
+            int userNameOneLength = userNameOne.Length;
+            int userNameTwoLength = userNameTwo.Length;
+
+            if (userNameOneLength > userNameTwoLength)
+            {
+                Console.WriteLine($"{userNameOne} is longer than {userNameTwo}.");
+            }
+            else
+            {
+                Console.WriteLine($"{userNameTwo} is longer than {userNameOne}.");
+            }
+        }
+    }
+}
