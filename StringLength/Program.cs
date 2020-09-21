262a57043744e4dc6eebae77bc202f9581ec492d
markusanton <markusa070@gmail.com>
2020-09-21 14:28:32 +0300

    Add project files.

---
+++ StringLength/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StringLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            int stringLength = userName.Length;
+            Console.WriteLine($"Your name is {stringLength} characters long.");
+           
+        }
+    }
+}
---
+++ StringsForLoops/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringsForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World!";
+            Console.WriteLine(helloWorld[0]);
+            Console.WriteLine(helloWorld[6]);
+
+            int stringLength = helloWorld.Length;
+            Console.WriteLine($"Your string contains: {stringLength} characters.");
+
+            char searchCriteria = 'o';
+            int i = 0;
+
+            foreach(char letter in helloWorld)
+            {
+                if (letter == searchCriteria)
+                {
+                    i++; 
+                }
+            }
+            Console.WriteLine($"There are {i} Os in your string.");
+        }
+    }
+}
